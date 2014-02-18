5021f0029329ebe6834c8370d71ca8128a349384
Kreeseeker1 <kreeseeker@gmail.com>
2014-02-17 23:42:33 -0800

    Second Overhaul

---
+++ Parts N Portals/src/com/sos2014/scene/SplashScene.java
@@ -0,0 +1,52 @@
+package com.sos2014.scene;
+
+import org.andengine.engine.camera.Camera;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.opengl.util.GLState;
+
+import com.sos2014.base.BaseScene;
+import com.sos2014.manager.SceneManager.SceneType;
+
+public class SplashScene extends BaseScene
+{
+	private Sprite splash;
+	
+	@Override
+	public void createScene()
+	{
+		splash = new Sprite(0, 0, resourcesManager.splash_region, vbom)
+    	{
+    		@Override
+            protected void preDraw(GLState pGLState, Camera pCamera) 
+    		{
+                super.preDraw(pGLState, pCamera);
+                pGLState.enableDither();
+            }
+    	};
+    	
+    	splash.setScale(1.5f);
+    	splash.setPosition(450, 240);
+    	attachChild(splash);
+	}
+
+	@Override
+	public void onBackKeyPressed()
+	{
+		return;
+	}
+
+	@Override
+	public SceneType getSceneType()
+	{
+		return SceneType.SCENE_SPLASH;
+	}
+
+	@Override
+	public void disposeScene()
+	{
+		splash.detachSelf();
+		splash.dispose();
+		this.detachSelf();
+		this.dispose();
+	}
+}
